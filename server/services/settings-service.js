bcf3f492ba182d875a5725f9578eb3331965eec0
daedalus <44623501+ComfortablyCoding@users.noreply.github.com>
2022-02-22 22:43:18 -0500

    refactor(settingsService): add build util

--- server/services/settings-service.js
+++ server/services/settings-service.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const _ = require('lodash');
 const { pluginId } = require('../utils/pluginId');
 
 module.exports = ({ strapi }) => ({
@@ -8,5 +9,35 @@ module.exports = ({ strapi }) => ({
 	},
 	set(settings) {
 		return strapi.config.set(`plugin.${pluginId}`, settings);
+	},
+	build(contentTypes) {
+		let models = {};
+
+		_.filter(strapi.contentTypes, (value, uid) => {
+			const model = contentTypes[value.modelName];
+			if (!model) {
+				return;
+			}
+
+			// ensure provided fields are present on the model
+			const hasField = _.get(value, ['attributes', model.field], false);
+			const hasReference = _.get(value, ['attributes', model.references], false);
+			if (!hasField || !hasReference) {
+				strapi.log.warn(
+					`[slugify] skipping ${value.info.singularName} registration, invalid field and/or reference provided.`
+				);
+				return;
+			}
+
+			const data = {
+				uid,
+				...contentTypes[value.modelName],
+				contentType: value,
+			};
+			models[uid] = data;
+			models[value.modelName] = data;
+		});
+
+		return models;
 	},
 });
