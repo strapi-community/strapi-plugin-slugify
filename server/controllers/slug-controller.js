367c9be875358e72af2f2f9d14ad5e78d173307e
daedalus <44623501+ComfortablyCoding@users.noreply.github.com>
2022-03-02 21:04:41 -0500

    refactor(slugController): utilise util functions

--- server/controllers/slug-controller.js
+++ server/controllers/slug-controller.js
@@ -1,11 +1,12 @@
 'use strict';
 
 const _ = require('lodash');
-const { sanitize, errors } = require('@strapi/utils');
+const { NotFoundError } = require('@strapi/utils/lib/errors');
 const { getPluginService } = require('../utils/getPluginService');
 const { transformResponse } = require('@strapi/strapi/lib/core-api/controller/transform');
-
-const { ValidationError, ForbiddenError } = errors;
+const { isValidFindSlugParams } = require('../utils/isValidFindSlugParams');
+const { sanitizeOutput } = require('../utils/sanitizeOutput');
+const { hasRequiredModelScopes } = require('../utils/hasRequiredModelScopes');
 
 module.exports = ({ strapi }) => ({
 	async findSlug(ctx) {
@@ -13,54 +14,35 @@ module.exports = ({ strapi }) => ({
 		const { modelName, slug } = ctx.request.params;
 		const { auth } = ctx.state;
 
-		try {
-			if (!modelName) {
-				throw new ValidationError('A model name path variable is required.');
-			}
+		isValidFindSlugParams({
+			modelName,
+			slug,
+			models,
+		});
 
-			if (!slug) {
-				throw new ValidationError('A slug path variable is required.');
-			}
+		const { uid, field, contentType } = models[modelName];
 
-			const model = models[modelName];
-			if (!model) {
-				throw new ValidationError(
-					`${modelName} model name not found, all models must be defined in the settings and are case sensitive.`
-				);
-			}
+		await hasRequiredModelScopes(strapi, uid, auth);
 
-			const { uid, field, contentType } = model;
+		// add slug filter to any already existing query restrictions
+		let query = ctx.query || {};
+		if (!query.filters) {
+			query.filters = {};
+		}
+		query.filters[field] = slug;
 
-			// Check if the user making the request has the
-			// `find` permission on the targeted model.
-			try {
-				await strapi.auth.verify(auth, { scope: `${uid}.findOne` });
-			} catch (e) {
-				throw new ForbiddenError();
-			}
+		// only return published entries by default if content type has draftAndPublish enabled
+		if (_.get(contentType, ['options', 'draftAndPublish'], false) && !query.publicationState) {
+			query.publicationState = 'live';
+		}
 
-			// add slug filter to any already existing query restrictions
-			let query = ctx.query || {};
-			if (!query.filters) {
-				query.filters = {};
-			}
-			query.filters[field] = slug;
+		const data = await getPluginService(strapi, 'slugService').findOne(uid, query);
 
-			// only return published entries by default if content type has draftAndPublish enabled
-			if (_.get(contentType, ['options', 'draftAndPublish'], false) && !query.publicationState) {
-				query.publicationState = 'live';
-			}
-
-			const data = await getPluginService(strapi, 'slugService').findOne(uid, query);
-
-			if (data) {
-				const sanitizedEntity = await sanitize.contentAPI.output(data, contentType, { auth });
-				ctx.body = transformResponse(sanitizedEntity);
-			} else {
-				ctx.notFound();
-			}
-		} catch (error) {
-			ctx.badRequest(error.message);
+		if (data) {
+			const sanitizedEntity = await sanitizeOutput(data, contentType, auth);
+			ctx.body = transformResponse(sanitizedEntity);
+		} else {
+			throw new NotFoundError();
 		}
 	},
 });
