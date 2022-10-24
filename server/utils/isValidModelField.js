99e63342e2378ae2638db27e6cfa07e66a98079b
daedalus <44623501+ComfortablyCoding@users.noreply.github.com>
2022-10-24 01:20:42 -0400

    feat(isValidModelField): add support for non model id field

--- server/utils/isValidModelField.js
+++ server/utils/isValidModelField.js
@@ -2,7 +2,8 @@
 
 const _ = require('lodash');
 
-const isValidModelField = (model, field) => _.get(model, ['attributes', field], false);
+const isValidModelField = (model, field) =>
+	_.get(model, ['attributes', field], false) || field === 'id';
 
 module.exports = {
 	isValidModelField,
