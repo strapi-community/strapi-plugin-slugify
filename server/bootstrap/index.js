6b3ec7169a3fa05ccce712d7a2af74f3eb39f108
daedalus <44623501+ComfortablyCoding@users.noreply.github.com>
2022-11-06 01:59:34 -0400

    fix(slugification): ensure correct count used for old plugin versions and projects with existing slugs

--- server/bootstrap/index.js
+++ server/bootstrap/index.js
@@ -2,8 +2,15 @@
 
 const { buildSettings } = require('./buildSettings');
 const { setupLifecycles } = require('./setupLifecycles');
+const { syncSlugCount } = require('./syncSlugCount');
 
-module.exports = async ({ strapi }) => {
-	const settings = await buildSettings(strapi);
+module.exports = async () => {
+	const settings = await buildSettings();
+
+	if (settings.slugifyWithCount) {
+		// Ensure correct count used for old plugin versions and projects with existing slugs.
+		await syncSlugCount(settings);
+	}
+
 	setupLifecycles(settings);
 };
